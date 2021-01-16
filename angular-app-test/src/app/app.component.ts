ad6451c2ea63cf3207d53097c73b0d3ffe913b0e
VenkataNagaraju Tippanaboina <vnagarajutweb@gmail.com>
2021-01-16 17:33:06 +0530

    componentinteraction
    Parent to Child using @Input
    Child to Parent using @Output by using events....

--- angular-app-test/src/app/app.component.ts
+++ angular-app-test/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'APP-Componet....';
+  public name = "from App componet VenkataNagaraju Tippanaboina...++++";
+  public message="";
 }
--- angular-app-test/src/app/componentinteraction/componentinteraction.component.ts
+++ angular-app-test/src/app/componentinteraction/componentinteraction.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input,Output ,EventEmitter } from '@angular/core';
+//import { EventEmitter } from 'events';
 
 @Component({
-  selector: '.app-componentinteraction',
+  selector: 'app-componentinteraction',
   template: `
-  <h2>ComponentInteraction......</h2>
+  <h2>ComponentInteraction @Input and @Output......  parent to child and chilld to parentt</h2>
 
-  
-  
+  <h2>{{"Hello ...." + parentData }}</h2>
+
+  <button (click)="fireEvent()">Send Event</button>
   `,
   styleUrls: ['./componentinteraction.component.css']
 })
 export class ComponentinteractionComponent implements OnInit {
 
+ @Input() public parentData="";
+  //@Input('parentData') public parentname;
+
+  @Output() public childEvent= new EventEmitter();
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fireEvent(){
+    this.childEvent.emit("Hi child component");
   }
 
 }
