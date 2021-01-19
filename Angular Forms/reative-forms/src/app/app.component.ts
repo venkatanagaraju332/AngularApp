5e44814bb260772e3dff4088473db7c1b94b0cbb
VenkataNagaraju Tippanaboina <vnagarajutweb@gmail.com>
2021-01-19 14:27:56 +0530

    worked with setvalue and patchvalue   in Reactive forms

--- Angular Forms/reative-forms/src/app/app.component.ts
+++ Angular Forms/reative-forms/src/app/app.component.ts
@@ -10,6 +10,28 @@ export class AppComponent {
   registraionForm = new FormGroup({
     userName : new FormControl('VenkataNagaraju'),
     password : new FormControl(''),
-    confirmPassword :new FormControl('')
+    confirmPassword :new FormControl(''),
+    address : new FormGroup({
+      city : new FormControl(''),
+      state : new FormControl(''),
+      postalCode : new FormControl('')
+    })
   });
+  loadApiData(){
+    /*this.registraionForm.setValue({
+      userName :'VenkataNagaraju',
+      password : 'test',
+      confirmPassword :'test',
+    address: {
+      city : 'Guntur',
+      state : 'A.P',
+      postalCode: '522413'
+      }
+    });*/
+    this.registraionForm.patchValue({
+      userName :'VenkataNagaraju',
+      password : 'test',
+      confirmPassword :'test'
+    }); 
+  }
 }
