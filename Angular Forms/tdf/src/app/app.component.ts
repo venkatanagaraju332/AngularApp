27619db379698e4db9723fca03f599475cfaa68d
VenkataNagaraju Tippanaboina <vnagarajutweb@gmail.com>
2021-01-19 10:01:19 +0530

    tdf example

--- Angular Forms/tdf/src/app/app.component.ts
+++ Angular Forms/tdf/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+  topics = ['Angular', 'React', 'Vue'];
+  topicHasError = true;
+  userModel= new User('Venkata','venkat@gmail.com',9030140332,'default','morning',true);
+  validateTopic(value: string){
+    if(value === 'default'){
+      this.topicHasError=true;
+    }else{
+      this.topicHasError=false;
+    }
+  }
 }
