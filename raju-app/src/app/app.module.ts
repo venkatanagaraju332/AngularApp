8755ac7fb2019b0c64e29ba1296c11b05bf131ff
VenkataNagaraju Tippanaboina <vnagarajutweb@gmail.com>
2021-04-25 11:38:44 +0530

    code removed

--- raju-app/src/app/app.module.ts
+++ raju-app/src/app/app.module.ts
@@ -4,24 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
-import { StudentListComponent } from './student-list/student-list.component';
-import { StudentDetailsComponent } from './student-details/student-details.component';
-import { StudentService } from './student.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
-    StudentListComponent,
-    StudentDetailsComponent
+    TestComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [
-    StudentService
-  ],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
